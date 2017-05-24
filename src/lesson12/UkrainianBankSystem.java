6290b4bf70bc9560ca05c2c96340ae58b0c91952
Sergio Talko <sergio.talko@gmail.com>
2017-05-24 14:05:38 +0300

    logic mistakes

--- src/lesson12/UkrainianBankSystem.java
+++ src/lesson12/UkrainianBankSystem.java
@@ -34,7 +34,7 @@ public class UkrainianBankSystem implements BankSystem {
 
     @Override
     public void paySalary(User user) {
-        user.setBalance(user.getBalance() + user.getSalary());
+        fund(user, user.getSalary());
     }
 
 
