68cc1fe51cbb3599e8c30e1ac5cc7856311b1699
Sergio Talko <sergio.talko@gmail.com>
2017-06-26 17:42:59 +0300

    homeWork lesson 17

--- src/lesson17/hw/Demo.java
+++ src/lesson17/hw/Demo.java
@@ -3,8 +3,14 @@ package lesson17.hw;
 public class Demo {
     public static void main(String[] args) {
         String str = "hhhh j jj j j jjjj jjj jjj";
+        String str2 = null;
+        String str3 = "HJJJJHJJJKklljkkfjkfkfkk KJfjjfjjckcjvjjvhjv";
+        String str4 = "         ";
         CountWords countWords = new CountWords();
         System.out.println(countWords.countWords(str));
+        System.out.println(countWords.countWords(str2));
+        System.out.println(countWords.countWords(str3));
+        System.out.println(countWords.countWords(str4));
 
     }
 }
