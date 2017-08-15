afcbc84da7c29c50412b7d15edd97426624f40cc
Sergio Talko <sergio.talko@gmail.com>
2017-08-15 21:48:52 +0300

    hw lesson 21

---
+++ src/lesson21/ArrayUtils.java
@@ -0,0 +1,17 @@
+package lesson21;
+
+public class ArrayUtils {
+
+    public static int maxElement(int[] array) {
+        return 0;
+    }
+    public static int minElement(int[] array) {
+        return 0;
+    }
+    public static int sumOfElement(int[] array) {
+        return 0;
+    }
+    public static int reverse(int[] array) {
+        return 0;
+    }
+}
