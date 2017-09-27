685a20ee9237876ff6d508be7d7bac8d6b87a476
Sergio Talko <sergio.talko@gmail.com>
2017-09-27 23:04:57 +0300

    fix nullPointer problem

--- src/lesson28/DateComparator.java
+++ src/lesson28/DateComparator.java
@@ -11,7 +11,8 @@ public class DateComparator implements Comparator<Capability>
     public int compare(Capability o1, Capability o2) throws NullPointerException {
 
         if (o1 == null || o2 == null) throw new NullPointerException("Please check input info.We have null there");
-        if (!o2.getDateCreated().equals(o1.getDateCreated())) {
+
+        if (o1.getDateCreated() != null && o2.getDateCreated() != null && !o2.getDateCreated().equals(o1.getDateCreated())) {
             return o2.getDateCreated().compareTo(o1.getDateCreated());
         }
         return 0;
--- src/lesson28/FullComparator.java
+++ src/lesson28/FullComparator.java
@@ -9,11 +9,11 @@ public class FullComparator implements Comparator<Capability> {
     public int compare(Capability o1, Capability o2) throws NullPointerException {
         if (o1 == null || o2 == null) throw new NullPointerException("Please check input info.We have null there");
 
-        if (!o1.getChannelName().equals(o2.getChannelName())) {
+        if (o1.getChannelName() != null && o2.getChannelName() != null && !o1.getChannelName().equals(o2.getChannelName())) {
             return o1.getChannelName().compareTo(o2.getChannelName());
-        } else if (!o1.getFingerPrint().equals(o2.getFingerPrint())) {
+        } else if (o1.getFingerPrint() != null && o2.getFingerPrint() != null && !o1.getFingerPrint().equals(o2.getFingerPrint())) {
             return o1.getFingerPrint().compareTo(o2.getFingerPrint());
-        } else if (!o2.getDateCreated().equals(o1.getDateCreated())) {
+        } else if (o1.getDateCreated() != null && o2.getDateCreated() != null && !o2.getDateCreated().equals(o1.getDateCreated())) {
             return o2.getDateCreated().compareTo(o1.getDateCreated());
         }
         return 0;
