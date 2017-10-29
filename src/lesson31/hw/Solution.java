bcfc05fe36c8c2e07351fdc8a3b6e6b02c3dc5b5
Sergio Talko <sergio.talko@gmail.com>
2017-10-29 10:50:05 +0200

    lesson 31 hw(Demo) add method countRepeat

--- src/lesson31/hw/Solution.java
+++ src/lesson31/hw/Solution.java
@@ -2,25 +2,24 @@ package lesson31.hw;
 
 import java.util.*;
 
-public class Solution {
+public class Solution<T> {
 
 
     public static Map countSymbols(String text) throws Exception {
 
         if (text == null || text.isEmpty()) throw new Exception("Input string is empty or null");
 
-        char[] chars = text.toCharArray();
 
+        Character[] chars = text.chars().mapToObj(c -> (char) c).toArray(Character[]::new);
 
         Map<Character, Integer> result = new Hashtable<>();
 
 
         for (Character character : chars) {
             if (Character.isLetter(character)) {
-                if (result.containsKey(character)) {
-                    result.put(character, result.get(character) + 1);
-                } else
-                    result.put(character, 1);
+                if (!result.containsKey(character)) {
+                    result.put(character, new Solution<Character>().countRepeat(chars, character));
+                }
             }
         }
 
@@ -46,7 +45,7 @@ public class Solution {
         for (String word : words) {
             if (word.length() > 2 && checkLetter(word)) {
                 if (result.containsKey(word)) {
-                    result.put(word, result.get(word) + 1);
+                    result.put(word, new Solution<String>().countRepeat(words, word));
                 } else
                     result.put(word, 1);
             }
@@ -69,6 +68,16 @@ public class Solution {
         return true;
     }
 
+    private int countRepeat(T[] t, T y) {
+
+        int count = 0;
+        for (T t1 : t) {
+            if (y.equals(t1)) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
  /* for (int i = 0; i < chars.length; i++) {
             if (Character.isLetter(chars[i])) {
