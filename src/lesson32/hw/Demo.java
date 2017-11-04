1ef65686875082125ec2505a002021544d301174
Sergio Talko <sergio.talko@gmail.com>
2017-11-04 16:31:25 +0200

    lesson 32 hw(Demo) change logic

--- src/lesson32/hw/Demo.java
+++ src/lesson32/hw/Demo.java
@@ -6,7 +6,6 @@ public class Demo {
     public static void main(String[] args) throws Exception {
 
         Solution.readNumbers();
-      //  ReadFromKeyboard.readKeyboardWithIOStream();
-     //   ReadFromKeyboard.readKeyboardWithScanner();
+
     }
 }
--- src/lesson32/hw/Solution.java
+++ src/lesson32/hw/Solution.java
@@ -11,34 +11,52 @@ public class Solution {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
 
-        int count = 2;
+        int count = 3;
         String inputString;
+        boolean checkValid = false;
         do {
-            System.out.println("Please enter 10 digits");
-            inputString = bufferedReader.readLine();
-            if (!validChecker(inputString))
-                System.out.println("You write " + inputString + "." + "Your numbers are wrong. You have " + count + " attempts to try again");
             count--;
-        } while (!validChecker(inputString)  && count >= 0);
+            System.out.println("Please enter 10 digits: ");
+            inputString = bufferedReader.readLine();
+
+            if (!validChecker(inputString)) {
+                System.out.println("You write " + inputString + "." + "Your numbers are wrong. You have " + count + " attempts to try again");
+            } else {
+                System.out.println("Sum of your digits is " + countDigit(inputString) + "!");
+                checkValid = true;
+
+            }
+        } while (!validChecker(inputString) && count > 0);
 
 
-        if (validChecker(inputString)){
-            System.out.println("Sum of your digits is " + countDigit(inputString) + "!");
-        }else System.out.println("Your numbers are wrong. Number of attempts exceeded");
+        if (!checkValid) System.out.println("Your numbers are wrong. Number of attempts exceeded");
+
+
     }
 
 
     private static boolean validChecker(String input) {
+
+        if (input.isEmpty() || input == null) return false;
+
+        while (input.contains("  "))
+            input = input.replaceAll("  ", " ");
 
         String[] digits = input.split(" ");
         if (digits.length != 10) return false;
 
         for (String s : digits) {
             for (Character character : s.toCharArray()) {
-                if (!Character.isDigit(character) || Integer.parseInt(s) > 100) {
+                if (!Character.isDigit(character)) {
                     return false;
 
                 }
+            }
+        }
+
+        for (String digit : digits) {
+            if (Integer.parseInt(digit) > 100) {
+                return false;
             }
         }
         return true;
