c9952089af7d9f55378900c258a03dfb704af177
Sergio Talko <sergio.talko@gmail.com>
2017-11-09 14:36:07 +0200

    lesson 33 hw(Demo) writer try-with-resources

--- src/lesson33/hw/SolutionWriter.java
+++ src/lesson33/hw/SolutionWriter.java
@@ -10,20 +10,15 @@ public class SolutionWriter {
 
     public static void writeToFileFromConsole(String path) {
 
-       if (!checkPath(path)){
-           System.err.println("File with path " + path + " not found");
-           return;
+        if (!checkPath(path)) {
+            System.err.println("File with path " + path + " not found");
+            return;
         }
 
         ArrayList<String> arrayOfInputsStrings = inputDataArray();
 
 
-        FileWriter fileWriter = null;
-        BufferedWriter bufferedWriter = null;
-        try {
-            fileWriter = new FileWriter(path, true);
-            bufferedWriter = new BufferedWriter(fileWriter);
-
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(path, true))) {
 
             for (String s : arrayOfInputsStrings) {
                 if (s != null) {
@@ -31,29 +26,21 @@ public class SolutionWriter {
                     bufferedWriter.newLine();
                 }
             }
-
-
         } catch (IOException e) {
             System.err.println("Cant write to file");
             return;
-        } finally {
-            IOUtils.closeQuietly(bufferedWriter);
-            IOUtils.closeQuietly(fileWriter);
-
         }
     }
 
     private static ArrayList<String> inputDataArray() {
         String write = "wr";
         String inputData = "";
-        BufferedReader bufferedReader = null;
-        InputStreamReader inputStreamReader = null;
+
         ArrayList<String> arrayOfInputsStrings = new ArrayList<>();
 
 
-        try {
-            inputStreamReader = new InputStreamReader(System.in);
-            bufferedReader = new BufferedReader(inputStreamReader);
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
+
             System.out.println("Enter file content to write in the file:");
 
             while (!inputData.equals(write)) {
@@ -65,22 +52,19 @@ public class SolutionWriter {
 
         } catch (IOException e) {
             System.err.println("Reading from keyboard failed");
-        } finally {
-            IOUtils.closeQuietly(inputStreamReader);
-            IOUtils.closeQuietly(bufferedReader);
         }
+
         return arrayOfInputsStrings;
     }
 
     private static boolean checkPath(String path) {
-        FileReader fileReader = null;
-        try {
-            fileReader = new FileReader(path);
+
+        try (FileReader fileReader = new FileReader(path)) {
             return true;
         } catch (FileNotFoundException e) {
             return false;
-        } finally {
-            IOUtils.closeQuietly(fileReader);
+        } catch (IOException e) {
+            return false;
         }
     }
 
